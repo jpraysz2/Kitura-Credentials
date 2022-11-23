6975800a0c5cc9ebd5f17e65fbe13f13953ad99a
Jean-Pierre Raysz <jean-pierre.raysz@everial.com>
2022-11-23 14:14:02 +0100

    KituraJP

--- Package.swift
+++ Package.swift
@@ -29,7 +29,7 @@ let package = Package(
         )
     ],
     dependencies: [
-      .package(url: "https://github.com/jpraysz2/Kitura-Session.git", from: "1.0.0"),
+      .package(url: "https://github.com/jpraysz2/Kitura-Session.git", from: "1.0.1"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
--- Sources/Credentials/Credentials.swift
+++ Sources/Credentials/Credentials.swift
@@ -14,7 +14,7 @@
  * limitations under the License.
  **/
 
-import Kitura
+import KituraJP
 import KituraNet
 import KituraSession
 import LoggerAPI
